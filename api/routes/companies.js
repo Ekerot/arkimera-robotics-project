fa6c50556367605d4db564c26e86542ddd42594c
beppek <beppe@live.se>
2017-04-18 19:43:39 +0200

    mocks bookkeep receipt endpoint

--- api/routes/companies.js
+++ api/routes/companies.js
@@ -31,4 +31,46 @@ router.get('/companies', (req, res) => {
     );
 });
 
+router.put('/companies/:companyID/files/:fileID/receipts', (req, res) => {
+    console.log(req.body);
+    res.status(200).send(
+        {
+            "success": true,
+            "data": {
+                "verificationSerie": "A",
+                "description": "Maxi ICA Stormarknad",
+                "receiptDate": "2016-06-03",
+                "accounts": [
+                {
+                    "account": "1930",
+                    "debit": "0.00",
+                    "credit": "272.18"
+                },
+                {
+                    "account": "7600",
+                    "debit": "241.24",
+                    "credit": "0.00"
+                },
+                {
+                    "account": "2641",
+                    "debit": "28.94",
+                    "credit": "0.00"
+                },
+                {
+                    "account": "6100",
+                    "debit": "1.60",
+                    "credit": "0.00"
+                },
+                {
+                    "account": "2641",
+                    "debit": "0.40",
+                    "credit": "0.00"
+                }
+                ]
+            },
+            "time": "2017-03-28 11:41:02"
+        }
+    )
+});
+
 module.exports = router;
