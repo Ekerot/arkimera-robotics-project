0f3152eda089978910ccb7c0b0ff3725cec876d9
beppek <beppe@live.se>
2017-04-18 17:40:32 +0200

    the end of .net

---
+++ api/ArkimeraDemoAPI/ArkimeraDemoAPI/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace ArkimeraDemoAPI.Controllers
+{
+    [Route("companies/{companyID}/[controller]")]
+    public class FilesController : Controller
+    {
+        [HttpPost()]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            long size = file.Length;
+
+            var filePath = Path.GetTempFileName();
+            
+            if (size > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            return Ok(new { size, filePath });
+        }
+    }
+}
