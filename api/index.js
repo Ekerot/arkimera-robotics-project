6a7283b1809e06d1f139f02427be8a89d0e3f48a
Nicklas Björkendal <nb222gp@student.lnu.se>
2017-04-19 17:27:08 +0800

    jwt now implemented

--- api/index.js
+++ api/index.js
@@ -4,14 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const api = express();
 
+const jwtAuth = require("./jwtAuth");
+
 // -- IMPORT ROUTES -- \\
 const pingRoutes = require('./routes/ping');
 const authRoutes = require('./routes/auth');
 const filesRoutes = require('./routes/files');
 const companiesRoutes = require('./routes/companies');
 
+// -- MIDDLEWARE -- \\
 api.use(bodyParser.urlencoded({extended: true}));
 api.use(bodyParser.json());
+api.use(jwtAuth.checkAuth); //checks body so must be after bodyparser
 
 // -- CORS -- \\
 api.use(function(req, res, next) {
@@ -21,11 +25,13 @@ api.use(function(req, res, next) {
     next();
 });
 
+api.set("x-powered-by", false); //set so api do not leak implementation details
+
 //  -- ROUTING -- \\
 api.use('/', pingRoutes);
 api.use('/', authRoutes);
-api.use('/', filesRoutes);
-api.use('/', companiesRoutes);
+api.use('/', jwtAuth.requireAuth, filesRoutes);
+api.use('/', jwtAuth.requireAuth, companiesRoutes);
 
 const port = process.env.PORT || 8080;
 
