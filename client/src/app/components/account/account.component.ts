652efa8c1cad7490d9cf19b23d664eb6eebec589
Ekerot <daniel.ekerot@gmail.com>
2017-04-24 13:34:56 +0200

    client view account features

--- client/src/app/components/account/account.component.ts
+++ client/src/app/components/account/account.component.ts
@@ -1,30 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
-
-  public myForm: FormGroup;
-  public invoiceItemsData = [{
-
-
-
-  }];
   public selectedValue: string;
+  public myForm: FormGroup;
+  public invoiceItemsData: object[] = [{}];
+  public totalAmount: any = 0;
+  public oldValue: number = 0;
+  public newValue: number;
 
   public accounts = [
+    { value: '', viewValue: '' },
     { value: '1910', viewValue: 'Kassa' },
     { value: '1920', viewValue: 'Plusgiro' },
     { value: '1930', viewValue: 'Bankkonto' },
-    { value: '2611', viewValue: 'Utående moms försäljning i Sverige 25%' },
-    { value: '2621', viewValue: 'Utående moms försäljning i Sverige 12%' },
     { value: '2641', viewValue: 'Ingående moms' },
-    { value: '3052', viewValue: 'Försäljn momspliktiga varor Sverige 25%' },
-    { value: '3051', viewValue: 'Försäljn momspliktiga varor Sverige 12%' },
     { value: '3740', viewValue: 'Öresutjämning' },
     { value: '5611', viewValue: 'Drivmedel personbilar' },
     { value: '5890', viewValue: 'Övriga resekostnader' },
@@ -41,8 +36,33 @@ export class AccountComponent implements OnInit {
     this.invoiceItemsData.push({});
   }
 
-  deleteRow(id) {
-    console.log('Delete: ' + id)
+  deleteRow() {
+    console.log('Delete: ' + this.invoiceItemsData);
+    this.invoiceItemsData.find;
   }
 
+  update() {
+
+    this.totalAmount = 0;
+
+    for (let object of this.invoiceItemsData) {
+
+      console.debug(object);
+
+      if (object["amount"] != null) {
+
+        if (object["value"] === '3740' || '1910' || '1920') {
+
+          this.totalAmount -= object["amount"];
+
+        }
+        else {
+
+          this.totalAmount += object["amount"];
+
+        }
+      }
+    }
+  }
 }
+
