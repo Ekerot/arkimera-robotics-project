89e7764806f0dfa04d9ff08bb6922b7dd3b375b6
Nicklas Björkendal <nb222gp@student.lnu.se>
2017-04-26 15:07:59 +0800

    Added 404 and error handling

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const api = express();
-const cors = require("cors");
+const moment = require('moment');
 
-const jwtAuth = require("./jwtAuth");
+const api = express();
+const cors = require('cors');
+
+const jwtAuth = require('./jwtAuth');
 
 // -- IMPORT ROUTES -- \\
 const pingRoutes = require('./routes/ping');
@@ -14,21 +14,51 @@ const filesRoutes = require('./routes/files');
 const companiesRoutes = require('./routes/companies');
 
 // -- MIDDLEWARE -- \\
-api.use(bodyParser.urlencoded({extended: true}));
+api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
-api.use(jwtAuth.checkAuth); //checks body so must be after bodyparser
+api.use(jwtAuth.checkAuth); // checks body so must be after bodyparser
 api.use(cors());
 
-api.set("x-powered-by", false); //set so api do not leak implementation details
+api.set('x-powered-by', false); // set so api do not leak implementation details
 
 //  -- ROUTING -- \\
 api.use('/', pingRoutes);
 api.use('/', authRoutes);
-api.use('/', jwtAuth.requireAuth, filesRoutes);
-api.use('/', jwtAuth.requireAuth, companiesRoutes);
+api.use('/companies', jwtAuth.requireAuth, filesRoutes);
+api.use('/companies', jwtAuth.requireAuth, companiesRoutes);
+
+// General 404 error is not specified in AzoraOne API documentation,
+// but this is their response
+api.use((req, res) => {
+  res.status(404).json({
+    statusCode: 404,
+    message: 'Resource not found',
+  });
+});
+
+// Express error middleware must have 4 args,
+// so do not remove unused parameters even if eslint complains
+api.use((error, req, res, next) => {
+  let payload;
+  if (Object.prototype.hasOwnProperty.call(error, 'payload')) {
+    payload = error.payload;
+  } else {
+    payload = {
+      success: false,
+      data: [{
+        code: 0,
+        message: error.message,
+        details: '',
+        element: '',
+      }],
+      time: moment().format('YYYY-MM-DD hh:mm:ss'),
+    };
+  }
+  res.status(error.status).json(payload);
+});
 
 const port = process.env.PORT || 8080;
 
 api.listen(port, () => {
-    console.log(`API listening on http://localhost:${port}`);
+  console.log(`API listening on http://localhost:${port}`);
 });
