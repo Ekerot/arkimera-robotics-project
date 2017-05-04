6f31f359cf8b3e62ed844deeddaa375ccb001b0e
Ekerot <daniel.ekerot@gmail.com>
2017-05-04 21:16:25 +0200

    Complete redoing of expenses view

--- client/src/app/components/pdf-handling/account/account.component.ts
+++ client/src/app/components/pdf-handling/account/account.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+
+
+import { MdSnackBar } from '@angular/material';
+import { ExtractResponse } from 'app/_models';
 
 @Component({
   selector: 'app-account',
@@ -8,58 +12,101 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AccountComponent implements OnInit {
 
-  public selectedValue: string;
-  public myForm: FormGroup;
-  public invoiceItemsData: object[] = [{}];
-  public totalAmount: number;
-  public accounts = [
-    { value: '', viewValue: '' },
-    { value: '1910', viewValue: 'Kassa' },
-    { value: '1920', viewValue: 'Plusgiro' },
-    { value: '1930', viewValue: 'Bankkonto' },
-    { value: '2641', viewValue: 'Ingående moms' },
-    { value: '3740', viewValue: 'Öresutjämning' },
-    { value: '5611', viewValue: 'Drivmedel personbilar' },
-    { value: '5890', viewValue: 'Övriga resekostnader' },
-    { value: '6071', viewValue: 'Representation, avdragsgill' },
-    { value: '6072', viewValue: 'Representation, ej avdragsgill' },
-    { value: '6110', viewValue: 'Kontorsmaterial' },
-  ];
-  public date: Date;
+  public testData: ExtractResponse[] = [{
+    success: true,
+    data: {
+      verificationSerie: 'A',
+      description: 'Hej hej',
+      receiptDate: new Date(2017, 4, 2),
+      accounts:
+      [
+        {
+          account: 1930,
+          debit: 0.00,
+          credit: 123.00
+        },
+        {
+          account: 4323,
+          debit: 1234.00,
+          credit: 0.00
+        },
+        {
+          account: 4323,
+          debit: 1234.00,
+          credit: 0.00
+        },
+      ]
+    },
+    time: new Date(2017, 4, 27, 12, 32)
+  }];
+
+  public accountForm: FormGroup;
+  public totalAmount: number = 0;
   public memo: string;
 
-  constructor(private formBuilder: FormBuilder) { }
+  /*public accounts = [
+    { value: '', viewValue: '' },
+    { value: 1910, viewValue: 'Kassa' },
+    { value: 1920, viewValue: 'Plusgiro' },
+    { value: 1930, viewValue: 'Bankkonto' },
+    { value: 2641, viewValue: 'Ingående moms' },
+    { value: 3740, viewValue: 'Öresutjämning' },
+    { value: 5611, viewValue: 'Drivmedel personbilar' },
+    { value: 5890, viewValue: 'Övriga resekostnader' },
+    { value: 6071, viewValue: 'Representation, avdragsgill' },
+    { value: 6072, viewValue: 'Representation, ej avdragsgill' },
+    { value: 6110, viewValue: 'Kontorsmaterial' },
+  ];*/
 
-  ngOnInit() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    public snackBar: MdSnackBar) { }
 
-  addNewRow() {
-    this.invoiceItemsData.push({});
+  ngOnInit() {
+
+    this.accountForm = this.formBuilder.group({
+
+      verificationSerie: [this.testData[0].data.verificationSerie, Validators.required],
+      description: [this.testData[0].data.description],
+      receiptDate: [this.testData[0].data.receiptDate, Validators.required],
+      accounts: this.formBuilder.array([
+      ])
+    })
+
+    for (const account of this.testData[0].data.accounts) {
+      const control = <FormArray>this.accountForm.controls['accounts'];
+    control.push(this.initAccount( account.account, account.debit, account.credit ));
+    }
+
   }
 
-  deleteRow(value: number) {
+  initAccount( account: number,debit: number, credit: number ) {
 
-    const index: number = value;
-
-    if (index !== -1) {
-      this.invoiceItemsData.splice(index, 1);
-    }
+    return this.formBuilder.group({
+      account: [account, Validators.required],
+      debit: [debit],
+      credit: [credit]
+    });
   }
 
-  update() {
+  addAccount() {
+    // add address to the list
+    const control = <FormArray>this.accountForm.controls['accounts'];
+    control.push(this.initAccount( null, null, null ));
+  }
 
-    const val: number[] = [3740, 1910, 1920, 1930, 3740];
-    this.totalAmount = 0;
+  deleteAccount(value: number) {
+    // remove address from the list
+    const control = <FormArray>this.accountForm.controls['accounts'];
+    control.removeAt(value);
+  }
 
-    for (const item of this.invoiceItemsData) {
-
-      if (item['amount'] != null) {
-        if (val.indexOf(parseInt(item['selectedValue'], 10)) > -1) {
-          this.totalAmount -= item['amount'];
-        } else {
-          this.totalAmount += item['amount'];
-        }
-      }
-    }
+  openSnackBar(message: string): void {
+    this.snackBar.open(message, 'Close', {
+      duration: 5000,
+    });
   }
 }
+
+
 
