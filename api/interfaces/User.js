8e4aaa19be3e2ab53df31112fe85f3dd5ed8b61b
Nicklas Björkendal <nb222gp@student.lnu.se>
2017-05-05 18:23:07 +0800

    Finished seed data

--- api/interfaces/User.js
+++ api/interfaces/User.js
@@ -1,4 +1,4 @@
-const User = require('../models/User.js');
+const User = require('../models/MongoUser.js');
 
 module.exports = {
   addNew: (user, callback) => {
--- api/seedData.js
+++ api/seedData.js
@@ -1,17 +1,12 @@
-const db = require('./config/mongoose.js');
-const User = require('./models/User.js');
+const mongoose = require('./config/mongoose.js');
+const User = require('./interfaces/User.js');
 
-db();
+const db = mongoose();
 
 const userDetails = { username: 'admin', password: 'admin' };
-
-const user = new User({ userDetails });
-/*user.save((err, doc) => {
-  if (err) console.log(err);
-  if (doc) console.log(doc);
-});*/
 
 User.addNew(userDetails, (err, doc) => {
   if (err) console.log(err);
   if (doc) console.log(doc);
+  db.disconnect();
 });
