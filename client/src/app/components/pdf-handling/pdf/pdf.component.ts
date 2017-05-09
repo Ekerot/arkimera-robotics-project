7f0a39d190d1f2902ac75ce432412215b56316bd
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2017-05-09 20:41:31 +0200

    Saving of file information in localstorage...
    This does not work! Consider this commit a backup only!

--- client/src/app/components/pdf-handling/pdf/pdf.component.ts
+++ client/src/app/components/pdf-handling/pdf/pdf.component.ts
@@ -16,15 +16,23 @@ export class PdfComponent implements OnInit {
   };
   public file: File;
 
+  private fileContentsName: string;
+  private fileDataName: string;
+
   // TODO: How to handle more than one page? Vertical slider? How do we get numPages?
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {
+    this.fileContentsName = 'pdfContents';
+    this.fileDataName = 'pdfData';
+   }
 
   ngOnInit() {
-    const pdfData = localStorage.getItem('pdf');
+    const pdfContents = localStorage.getItem(this.fileContentsName);
+    const pdfData = localStorage.getItem(this.fileDataName);
 
-    if (pdfData) {
-      this.pdfOptions.data = pdfData;
+    if (pdfContents && pdfData) {
+      this.pdfOptions.data = pdfContents;
+      this.file = JSON.parse(pdfData) as File;
     }
   }
 
@@ -32,20 +40,26 @@ export class PdfComponent implements OnInit {
     this.file = (<HTMLInputElement>$event.target).files[0];
     const fileReader = new FileReader();
 
+    console.debug('File: ', this.file);
+    console.debug('File: ', JSON.stringify(this.file));
+
     fileReader.onloadend = (e) => {
       this.pdfOptions.data = fileReader.result;
-      localStorage.setItem('pdf', fileReader.result);
+      localStorage.setItem(this.fileContentsName, fileReader.result);
+      localStorage.setItem(this.fileDataName, JSON.stringify(this.file));
     };
 
     fileReader.readAsBinaryString(this.file);
   }
 
   onUpload(): void {
-    this.httpService.uploadFile(this.file).subscribe(data => console.log('response', data));
+    this.httpService.uploadFile(this.file)
+    .subscribe(data => console.log('response', data));
   }
 
   onCancel(): void {
     this.pdfOptions.data = null;
-    localStorage.removeItem('pdf');
+    localStorage.removeItem(this.fileContentsName);
+    localStorage.removeItem(this.fileDataName);
   }
 }
