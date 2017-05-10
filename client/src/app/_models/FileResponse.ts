00b7f5b8d9cb61741565233b48d7fb4a56f2ffd3
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2017-05-10 19:37:36 +0200

    Added two new types/models for handling files from webAPI

---
+++ client/src/app/_models/FileResponse.ts
@@ -0,0 +1,11 @@
+export interface FileResponse {
+  _id: string,
+  username: string,
+  path: string,
+  originalname: string,
+  filename: string,
+  status: string,
+  FileID: number,
+  companyID: number,
+  __v: number
+}
--- client/src/app/_models/index.ts
+++ client/src/app/_models/index.ts
@@ -1,5 +1,7 @@
 export * from './Account';
 export * from './ApiResponse';
 export * from './Auth';
+export * from './FileId';
+export * from './FileResponse';
 export * from './ReceiptResponse';
 export * from './User';
