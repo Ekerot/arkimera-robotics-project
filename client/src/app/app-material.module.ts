a37877de9c6b0a5ae3bab83a8aed8b29f1d2405f
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2017-05-14 08:18:32 +0200

    Material buttons does not work anymore. Changed to bootstrap button styling

--- client/src/app/app-material.module.ts
+++ client/src/app/app-material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule, MdSnackBarModule, MdInputModule } from '@angular/material';
+import { MdButtonModule, MdSnackBarModule, MdInputModule, MdSliderModule } from '@angular/material';
 
 @NgModule({
-    imports: [MdButtonModule, MdSnackBarModule, MdInputModule],
-    exports: [MdButtonModule, MdSnackBarModule, MdInputModule],
+    imports: [MdButtonModule, MdSnackBarModule, MdInputModule, MdSliderModule],
+    exports: [MdButtonModule, MdSnackBarModule, MdInputModule, MdSliderModule],
 })
 export class AppMaterialModule { }
