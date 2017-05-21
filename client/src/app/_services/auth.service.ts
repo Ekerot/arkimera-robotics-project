2d7e5e7df7eb7025cee70767f7912053c1fd092e
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2017-05-21 16:39:26 +0200

    Changed how AuthService validates user logged in status. Now checks on demand and not when app is created. Also uses angular2-jwt

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { tokenNotExpired } from 'angular2-jwt';
 
 import { HttpService } from 'app/_services/http.service';
 
@@ -9,16 +10,18 @@ import { ApiResponse, User } from 'app/_models';
 @Injectable()
 export class AuthService {
 
-  public isLoggedIn: boolean;
-
   constructor(
     private http: HttpService,
     private router: Router
-  ) {
-    this.isLoggedIn = false;
+  ) { }
 
-    if (localStorage.getItem('token')) {
-      this.isLoggedIn = true;
+  isUserLoggedIn(): boolean {
+    try {
+      if (tokenNotExpired()) {
+        return true;
+      }
+    } catch (error) {
+      return false;
     }
   }
 
@@ -26,14 +29,12 @@ export class AuthService {
     return this.http.authenticate(user)
       .map(res => {
         localStorage.setItem('token', res.data.token);
-        this.isLoggedIn = true;
         return res;
       });
   }
 
   logout(): void {
     localStorage.removeItem('token');
-    this.isLoggedIn = false;
     this.router.navigate(['/login']);
   }
 }
