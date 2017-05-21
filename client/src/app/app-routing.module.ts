e32f5739088fbb7ae8321ab603ed803334eb1ba8
Tomas Vaedelund - tr222cj <tr222cj@student.lnu.se>
2017-05-21 16:45:22 +0200

    Removed HomeComponent since it's not used anymore.
    Fixes #92

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'app/_guards/auth.guard';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from 'app/components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ExpensesComponent } from 'app/components/pdf-handling/expenses/expenses.component';
